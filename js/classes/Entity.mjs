0b1d706a4a87c67a8a1165a61ccb6d7659c516ab
lukas stroomer <lukasstroomer@gmail.com>
2025-05-11 18:19:54 +0200

    add rotate translate draw optimizations

--- js/classes/Entity.mjs
+++ js/classes/Entity.mjs
@@ -1,5 +1,6 @@
 // Entity.mjs
 import { SCREEN_WIDTH, SCREEN_HEIGHT } from '../constants.mjs';
+import { drawPixelLine } from '../utils.mjs';
 
 export default class Entity {
   constructor({ name, x, y, dx, dy, size, angle }) {
@@ -10,44 +11,81 @@ export default class Entity {
     this.dy       = dy;         // float 
     this.size     = size;       // int
     this.halfsize = size / 2;   // int
-    this.angle    = angle;      // float
+    this.angle = angle;         // float
   }
 
   update(dt) {
+    // WRAP XY POSITION
     this.x = (this.x + this.dx * dt + SCREEN_WIDTH)  % SCREEN_WIDTH;
     this.y = (this.y + this.dy * dt + SCREEN_HEIGHT) % SCREEN_HEIGHT;
 
-    console.log(`player xy: [${this.x}, ${this.y}]`);
+    // ROTATE THE MODEL
+    this.rotate(this.angle);   
+    // TRANSLATE THE MODEL
+    this.translate(this.x, this.y);
+  }
+
+  rotate(angle) {
+    for (let i = 0; i < this.vertices; i++) {
+      this.sx[i] = 10 * (this.mx[i] * Math.cos(angle) - this.my[i] * Math.sin(angle));
+      this.sy[i] = 10 * (this.mx[i] * Math.sin(angle) + this.my[i] * Math.cos(angle));  
+    }
+  }
+
+  translate(x, y) {    
+    for (let i = 0; i < this.vertices; i++) {
+      this.sx[i] = Math.floor(this.sx[i] + x); 
+      this.sy[i] = Math.floor(this.sy[i] + y); 
+    }
+  }
+
+  render(ctx) {
+    for (let i = 0; i < this.vertices; i++) {
+      const x0 = this.sx[i];
+      const y0 = this.sy[i];
+      const x1 = this.sx[(i+1) % this.vertices];
+      const y1 = this.sy[(i+1) % this.vertices];
+
+      drawPixelLine(ctx, x0, y0, x1, y1);
+    }
     
+    ctx.fillStyle = 'red';
+    ctx.fillRect(Math.floor(this.x), Math.floor(this.y), 1, 1);
   }
 
   draw(ctx) {    
-    const { x, y, halfsize } = this;
+    
+    
+  
+    //const { x, y, halfsize } = this;
 
     // Draw the main entity
-    this.render(ctx, x - halfsize, y - halfsize, 'yellow');
+    //this.render(ctx, 'yellow');
 
-    const nearLeft   = x - halfsize < 0;
-    const nearRight  = x + halfsize > SCREEN_WIDTH;
-    const nearTop    = y - halfsize < 0;
-    const nearBottom = y + halfsize > SCREEN_HEIGHT;
+    //ctx.fillStyle = 'red';
+    //ctx.fillRect(x, y, 1, 1);
 
-    // Horizontal wraps
-    if (nearLeft)   this.render(ctx, x - halfsize + SCREEN_WIDTH, y - halfsize);
-    if (nearRight)  this.render(ctx, x - halfsize - SCREEN_WIDTH, y - halfsize);
+    // const nearLeft   = x - halfsize < 0;
+    // const nearRight  = x + halfsize > SCREEN_WIDTH;
+    // const nearTop    = y - halfsize < 0;
+    // const nearBottom = y + halfsize > SCREEN_HEIGHT;
 
-    // Vertical wraps
-    if (nearTop)    this.render(ctx, x - halfsize, y - halfsize + SCREEN_HEIGHT);
-    if (nearBottom) this.render(ctx, x - halfsize, y - halfsize - SCREEN_HEIGHT);
+    // // Horizontal wraps
+    // if (nearLeft)   this.render(ctx, x - halfsize + SCREEN_WIDTH, y - halfsize);
+    // if (nearRight)  this.render(ctx, x - halfsize - SCREEN_WIDTH, y - halfsize);
 
-    // Diagonal corners (only if necessary)
-    if (nearLeft && nearTop)          this.render(ctx, x - halfsize + SCREEN_WIDTH, y - halfsize + SCREEN_HEIGHT);
-    else if (nearLeft && nearBottom)  this.render(ctx, x - halfsize + SCREEN_WIDTH, y - halfsize - SCREEN_HEIGHT);
-    else if (nearRight && nearTop)    this.render(ctx, x - halfsize - SCREEN_WIDTH, y - halfsize + SCREEN_HEIGHT);
-    else if (nearRight && nearBottom) this.render(ctx, x - halfsize - SCREEN_WIDTH, y - halfsize - SCREEN_HEIGHT);
+    // // Vertical wraps
+    // if (nearTop)    this.render(ctx, x - halfsize, y - halfsize + SCREEN_HEIGHT);
+    // if (nearBottom) this.render(ctx, x - halfsize, y - halfsize - SCREEN_HEIGHT);
+
+    // // Diagonal corners (only if necessary)
+    // if (nearLeft && nearTop)          this.render(ctx, x - halfsize + SCREEN_WIDTH, y - halfsize + SCREEN_HEIGHT);
+    // else if (nearLeft && nearBottom)  this.render(ctx, x - halfsize + SCREEN_WIDTH, y - halfsize - SCREEN_HEIGHT);
+    // else if (nearRight && nearTop)    this.render(ctx, x - halfsize - SCREEN_WIDTH, y - halfsize + SCREEN_HEIGHT);
+    // else if (nearRight && nearBottom) this.render(ctx, x - halfsize - SCREEN_WIDTH, y - halfsize - SCREEN_HEIGHT);
   }
 
-  render(ctx, x, y) {
-    throw new Error('Subclasses must implement render()');
-  }
+  // render(ctx, x, y) {
+  //   throw new Error('Subclasses must implement render()');
+  // }
 }
--- js/classes/Player.mjs
+++ js/classes/Player.mjs
@@ -2,30 +2,33 @@
 import Entity from './Entity.mjs';
 import { P1_COLOR } from '../constants.mjs';
 import { isDown, isUp } from '../keyboard.mjs';
-import { drawPixelLine } from '../utils.mjs';
-
 
 export default class Player extends Entity {
   constructor({ name, x, y, dx, dy, size, keys, debug }) {
     super({ name, x, y, dx, dy, size, angle:0.0 });
       
-    this.keys = keys;
+    this.keys  = keys;
     this.debug = debug;
     
+    this.accel = 40.0;
+    this.mx    = [0.0, -2.5, 2.5];
+    this.my    = [-5.5, 2.5, 2.5];
+    this.sx    = [];
+    this.sy    = [];
+    this.vertices = this.mx.length;
+
   }
 
   update(dt) {
-    const accel = 40.0;
-
     if (isDown(this.keys.up)) {
       // ACCELERATION changes VELOCITY with respect to TIME
-      this.dx +=  Math.sin(this.angle) * accel * dt;
-      this.dy += -Math.cos(this.angle) * accel * dt;
+      this.dx +=  Math.sin(this.angle) * this.accel * dt;
+      this.dy += -Math.cos(this.angle) * this.accel * dt;
     }
     // VELOCITY changes POSITION with respect to TIME
     if (isDown(this.keys.down)) {
-      this.dx -=  Math.sin(this.angle) * (accel/2) * dt;
-      this.dy -= -Math.cos(this.angle) * (accel/2) * dt;
+      this.dx -=  Math.sin(this.angle) * (this.accel/2) * dt;
+      this.dy -= -Math.cos(this.angle) * (this.accel/2) * dt;
     }
 
     if (isDown(this.keys.left))   this.angle -= 5.0 * dt;
@@ -42,46 +45,18 @@ export default class Player extends Entity {
       console.log('enter');    
     }  
     
-      
     super.update(dt);
   }
 
-  render(ctx, x, y, color) {
-    // ctx.fillStyle = color;
-    // ctx.fillRect(Math.floor(x), Math.floor(y), this.size, this.size);
-
-    // rotate
-    // translate 
-    // draw polygon 
-    const { angle } = this;  
-    const mx = [0.0, -2.5, 2.5];
-    const my = [ -5.5, 2.5, 2.5];
-    let sx = [];
-    let sy = [];
-    const scale = 8;
-
-    // ROTATE THE MODEL
-    for (let i = 0; i < 3; i++) {
-      sx[i] = scale * (mx[i] * Math.cos(angle) - my[i] * Math.sin(angle));
-      sy[i] = scale * (mx[i] * Math.sin(angle) + my[i] * Math.cos(angle));  
-    }
-
-    // TRANSLATE THE MODEL
-    for (let i = 0; i < 3; i++) {
-      sx[i] = Math.floor(sx[i] + x); 
-      sy[i] = Math.floor(sy[i] + y); 
-    }
-
+  render(ctx, color) {
     ctx.fillStyle = color;
+    super.render(ctx);
+  }
 
-    // DRAW THE MODEL
-    for (let i = 0; i < 3; i++) {
-      const x0 = sx[i];
-      const y0 = sy[i];
-      const x1 = sx[(i+1) % 3];
-      const y1 = sy[(i+1) % 3];
+  draw(ctx) {
+    this.render(ctx, P1_COLOR);
+    super.draw();
 
-      drawPixelLine(ctx, x0, y0, x1, y1);
-    }
+    
   }
 }
